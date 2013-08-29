f9d04ae39a88459a1c63d4b5ee6e20e32cb25e5c
James Moo <jamesm@channelgrabber.com>
2013-08-29 16:04:25 +0100

    remove unecessary setting which will probably be included elsewhere

--- recipes/php.rb
+++ recipes/php.rb
@@ -31,8 +31,6 @@ end
 
 # Install gearman pecl package
 php_pear "gearman" do
-  channel 'pecl.php.net'
-  version '1.1.2'
   action :install
   not_if "php --info | grep -qs 'gearman support => enabled'"
 end
