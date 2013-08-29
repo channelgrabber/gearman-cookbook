76501cb80e799d54d18b4ab0953d6b99a0b12048
James Moo <jamesm@channelgrabber.com>
2013-08-29 15:37:13 +0100

    pear autodiscover

--- recipes/php.rb
+++ recipes/php.rb
@@ -29,6 +29,9 @@ php_pear_channel 'pecl.php.net' do
   action :update
 end
 
+execute "enable pear auto discover" do
+    command "pear config-set auto_discover 1"
+end
 # Install gearman pecl package
 php_pear "gearman" do
   version node['gearman']['php']['version']
